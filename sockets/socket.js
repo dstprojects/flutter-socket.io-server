b47411c6168dc9b85566a1094c3370530bb242f5
dstprojects <36209165+dstprojects@users.noreply.github.com>
2020-11-04 01:22:23 -0600

    Cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,20 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand( new Band('Queen') );
+bands.addBand( new Band('Bon Jovi') );
+bands.addBand( new Band('Heroes del silencio') );
+bands.addBand( new Band('Metallica') );
 
 // Mensajes de Sockets
 io.on('connection', client => {
 
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands() );
 
     client.on('disconnect', () => { 
         console.log('Cliente desconectado');
@@ -13,6 +24,24 @@ io.on('connection', client => {
         console.log('Mensaje', payload.nombre);
 
         io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
-    })
+    });
+
+    client.on('vote-band', (payload) => {
+
+        bands.voteBand( payload.id );
+        io.emit('active-bands', bands.getBands() );
+
+    });
+
+    client.on('add-band', (payload) => {
+        bands.addBand( new Band(payload['name']) );
+        io.emit('active-bands', bands.getBands() );
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand( payload.id );
+        io.emit('active-bands', bands.getBands() );
+    });
+    
 
 });
